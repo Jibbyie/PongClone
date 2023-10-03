6786e9247a5f2e1b704b3091828826df9421bdc7
Jibbyie <jibbyie@gmail.com>
2023-10-03 13:16:08 +0100

    Initial commit - basic assets

---
+++ Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+    Rigidbody2D playerRB;
+    public float movementSpeed = 5f;    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * 0, Input.GetAxisRaw("Vertical") * movementSpeed);
+    }
+}
