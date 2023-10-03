8f762853f2e878e2e0c11d766b177cf2289cf604
Jibbyie <jibbyie@gmail.com>
2023-10-03 17:40:33 +0100

    Changed BallMovement script to be called InitialBallSpawner, added a short delay before the ball starts moving and sfx to indicate a 3 second countdown

--- Assets/Scripts/InitialBallSpawner.cs
+++ Assets/Scripts/InitialBallSpawner.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class BallMovement : MonoBehaviour
+public class InitialBallSpawner : MonoBehaviour
 {
     Rigidbody2D ballRB;
     public float spawnSpeed = 10f;
+    public float timeBeforeBallMoves = 3f;
+
+    public AudioSource gameStartSFX;
 
     // Start is called before the first frame update
     void Start()
+    {
+        gameStartSFX.Play();
+        Invoke("SpawnBall", timeBeforeBallMoves);
+    }
+
+    void SpawnBall()
     {
         float minAngle = -60f * Mathf.Deg2Rad; // Convert degrees to radians
         float maxAngle = 60f * Mathf.Deg2Rad;
@@ -18,8 +27,5 @@ public class BallMovement : MonoBehaviour
         ballRB = GetComponent<Rigidbody2D>();
         Vector2 randomDir = new Vector2(Mathf.Cos(ballAngle), Mathf.Sin(ballAngle));
         ballRB.velocity = randomDir * spawnSpeed;
-
     }
-
-
 }
